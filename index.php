be32f4a5c043447d6a9e18890eb629faeefeb33c
FR_Jess <fr_jess@icloud.com>
2022-12-02 10:40:01 +0100

    [add] filters

--- index.php
+++ index.php
@@ -40,6 +40,34 @@ $hotels = [
 
 ];
 
+//di default l'elenco filtrato è l'elenco pieno
+$filtered_hotels = $hotels;
+
+if (!empty($_GET['parking'])) {
+  $temp_hotels = [];
+  foreach ($filtered_hotels as $hotel) {
+    if ($hotel['parking']) $temp_hotels[] = $hotel;
+  }
+  $filtered_hotels = $temp_hotels;
+}
+
+//se ho scelto senza parcheggio verifico l'esistenza del parametro parking in GET e che sia vuoto
+if (isset($_GET['parking']) && empty($_GET['parking'])) {
+  $temp_hotels = [];
+  foreach ($filtered_hotels as $hotel) {
+    if (!$hotel['parking']) $temp_hotels[] = $hotel;
+  }
+  $filtered_hotels = $temp_hotels;
+}
+
+if (!empty($_GET['vote'])) {
+  $temp_hotels = [];
+  foreach ($filtered_hotels as $hotel) {
+    if ($hotel['vote'] >= $_GET['vote']) $temp_hotels[] = $hotel;
+  }
+  $filtered_hotels = $temp_hotels;
+}
+
 ?>
 
 
@@ -57,79 +85,76 @@ $hotels = [
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-  <title>Hotel</title>
+  <title>PHP Hotel</title>
 
 </head>
 
 <body>
-  <form class="row m-3">
-    <div class="col-4">
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-        <label class="form-check-label" for="inlineRadio1">Con parcheggio</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-        <label class="form-check-label" for="inlineRadio2">Senza parcheggio</label>
+
+  <div class="container py-4">
+
+    <div class="row mb-2">
+      <div class="col-12">
+
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" class="row">
+
+          <div class="col-sm-3">
+            <input class="form-check-input" type="radio" name="parking" id="parking1" value="">
+            <label class="form-check-label" for="parking1">Senza parcheggio</label>
+
+            <input class="form-check-input" type="radio" name="parking" id="parking2" value="1">
+            <label class="form-check-label" for="parking2">Con parcheggio</label>
+          </div>
+
+
+          <div class="col-sm-3">
+            <label for="inputVoto">Voto</label>
+            <input type="number" name="vote" id="vote">
+            <!-- <select class="form-select" aria-label="form-select example">
+              <option selected></option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select> -->
+          </div>
+
+          <div class="col-auto">
+            <button type="submit" class="btn btn-primary">Cerca</button>
+            <button type="button" class="btn btn-secondary">Annulla</button>
+          </div>
+
+        </form>
+
       </div>
     </div>
 
-    <div class="col-6">
-      <!-- <label for="inputVoto" class="col-sm-2 col-form-label">Voto</label> -->
-      <div class="col-sm-10">
-        <select class="form-select" aria-label="form-select example">
-          <option selected></option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-
-      </div>
-
-    </div>
-    <div class="col-2">
-      <button type="submit" class="btn btn-primary">Cerca</button>
-      <button type="button" class="btn btn-secondary">Annulla</button>
-    </div>
-
-  </form>
-
-  <table class="table table-warning table-striped m-3">
-    <thead>
-      <tr>
-        <th scope="col">Nome</th>
-        <th scope="col">Descrizione</th>
-        <th scope="col">Pacheggio</th>
-        <th scope="col">Stelle</th>
-        <th scope="col">Distanza dal centro</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($hotels as $hotel) : ?>
+    <table class="table table-warning table-striped">
+      <thead>
         <tr>
-          <td><?php echo $hotel['name'] ?></td>
-          <td><?php echo $hotel['description'] ?></td>
-          <td><?php echo $hotel['parking'] ? 'Si' : 'No' ?></td>
-          <td><?php echo $hotel['vote'] ?></td>
-          <td><?php echo $hotel['distance_to_center'] ?>km</td>
+          <th scope="col">Nome</th>
+          <th scope="col">Descrizione</th>
+          <th scope="col">Pacheggio</th>
+          <th scope="col">Stelle</th>
+          <th scope="col">Distanza dal centro</th>
         </tr>
-      <?php endforeach ?>
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        <?php foreach ($filtered_hotels as $hotel) : ?>
+          <tr>
+            <td><?php echo $hotel['name'] ?></td>
+            <td><?php echo $hotel['description'] ?></td>
+            <td><?php echo $hotel['parking'] ? 'Si' : 'No' ?></td>
+            <td><?php echo $hotel['vote'] ?></td>
+            <td><?php echo $hotel['distance_to_center'] ?>km</td>
+          </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+  </div>
+
 
 </body>
 
 </html>
-
-<!-- Stampare tutti i nostri hotel con tutti i dati disponibili.
-Iniziate in modo graduale.
-Prima stampate in pagina i dati, senza preoccuparvi dello stile.
-Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
-Bonus:
-1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
-2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
-NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
-Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
-Usiamo la logica con le nozioni che abbiamo fino ad ora senza cercare possibilità di filtri più evoluti che vedremo domani -->
